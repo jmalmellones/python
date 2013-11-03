3977a1ba135652b98ada83ff1d1f2e5979650495
jmalmellones <jmalmellones@gmail.com>
2013-11-03 14:59:53 +0100

    added a project to interface yahoo weather apis

---
+++ yahoo_weather/yahoo_weather/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'jmalmellones'
+import feedparser
+
+# returns current temperature in ºC in location
+#location for Mostoles, Madrid (Spain) = 767893
+
+
+def current_temperature(location):
+    d = feedparser.parse('http://weather.yahooapis.com/forecastrss?w=' + str(location) + '&u=c')
+    temperature = d['entries'][0]['yweather_condition']['temp']
+    return temperature
