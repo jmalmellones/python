c3f3e4c8c6954ba6877a4c255d9baff43241de09
jmalmellones <jmalmellones@gmail.com>
2013-11-07 20:40:22 +0100

    improvements in auto download from elitetorrent

--- download_file/download_file/__init__.py
+++ download_file/download_file/__init__.py
@@ -1,1 +1,38 @@
 __author__ = 'jmalmellones'
+import urllib2
+import urllib
+
+from HTMLParser import HTMLParser
+
+
+# create a subclass of HTMLParser and override the handler methods to retrieve 'magnet' urls
+class MyHTMLParser(HTMLParser):
+    en_a = False
+    url = ''
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            self.en_a = True
+            for attr, value in attrs:
+                if attr == 'href':
+                    self.url = value
+                    break
+
+    def handle_endtag(self, tag):
+        if tag == "a":
+            self.en_a = False
+
+    def handle_data(self, data):
+        # and data.startswith('Descargar')
+        if self.en_a and self.url.startswith('magnet'):
+            print self.url
+
+
+parser = MyHTMLParser()
+
+
+#descarga todos los magnet encontrados en una url dada
+def descargar_magnet_en_url(url):
+    e = urllib2.urlopen(url)
+    html = e.read()
+    parser.feed(html)
--- rss_downloader/rss_downloader/__init__.py
+++ rss_downloader/rss_downloader/__init__.py
@@ -1,7 +1,23 @@
 __author__ = 'jmalmellones'
 import feedparser
+import download_file
+import time
 
 
 d = feedparser.parse('http://www.elitetorrent.net/rss.php')
-print(d)
+
+if d['status'] == 200:
+    print('todo ok, vamos a leer el feed')
+    for entry in d['entries']:
+        titulo = entry['title_detail']['value']
+        url = entry['link']
+        print "descarga ", titulo, " en url ", url
+        try:
+            download_file.descargar_magnet_en_url(url)
+        except UnicodeDecodeError:
+            print "error bajando url ", url
+        time.sleep(5)
+else:
+    print('no hemos recibido un status 200, algo paso con el feed, saliendo...')
+
 
