db3c726915d05f555326051493a71e4929222959
jmalmellones <jmalmellones@gmail.com>
2013-11-24 20:55:38 +0100

    changed synology_client config file

--- synology_client/synology_client/__init__.py
+++ synology_client/synology_client/__init__.py
@@ -6,10 +6,10 @@ import json
 
 #uncomment to create config file
 #config = {'https': True, 'server': '192.168.1.1', 'port': 5001, 'user': 'admin', 'password': 'xxx'}
-#json.dump(config, open('config.json', 'w'))
+#json.dump(config, open('synology_client.json', 'w'))
 
 #load config
-config = json.load(open('config.json'))
+config = json.load(open('synology_client.json'))
 
 server = 'http'
 if config['https']:
@@ -19,12 +19,12 @@ server += '://' + config['server'] + ':' + str(config['port']) + '/webapi/'
 print('using server ' + server)
 
 
-def login(user, password, session_name):
-    if synology_is_online():
+def login(session_name):
+    if _synology_is_online():
         api_path = 'auth.cgi'
-        data_dict = {'api': 'SYNO.API.Auth', 'version': 2, 'method': 'login', 'account': user,
-                     'passwd': password, 'session': session_name, 'format': 'sid'}
-        result = synology_call(api_path, data_dict)
+        data_dict = {'api': 'SYNO.API.Auth', 'version': 2, 'method': 'login', 'account': config['user'],
+                     'passwd': config['password'], 'session': session_name, 'format': 'sid'}
+        result = _synology_call(api_path, data_dict)
         print(result)
         if result['success']:
             return result['data']['sid']
@@ -37,7 +37,7 @@ def login(user, password, session_name):
 def logout(session_name):
     api_path = 'auth.cgi'
     data_dict = {'api': 'SYNO.API.Auth', 'version': 2, 'method': 'logout', 'session': session_name}
-    result = synology_call(api_path, data_dict)
+    result = _synology_call(api_path, data_dict)
     if result['success']:
         print('logout ok')
     else:
@@ -48,7 +48,7 @@ def add_task(task_url, security_id):
     api_path = 'DownloadStation/task.cgi'
     data_dict = {'api': 'SYNO.DownloadStation.Task', 'version': 1, 'method': 'create',
                  'uri': task_url, '_sid': security_id}
-    result = synology_call(api_path, data_dict)
+    result = _synology_call(api_path, data_dict)
     if result['success']:
         print('task creation ok')
     else:
@@ -56,7 +56,7 @@ def add_task(task_url, security_id):
 
 
 #makes an api call to synology nas and returns response json
-def synology_call(api_path, data_dict):
+def _synology_call(api_path, data_dict):
     data = urllib.urlencode(data_dict)
     r = urllib2.urlopen(server + api_path, data)
     r2 = r.read()
@@ -64,10 +64,10 @@ def synology_call(api_path, data_dict):
 
 
 #returns true if synology is online
-def synology_is_online():
+def _synology_is_online():
     syno_api_path = 'query.cgi'
     data_dict = {'api': 'SYNO.API.Info', 'version': 1, 'method': 'query', 'query': 'SYNO.API.Auth'}
-    result = synology_call(syno_api_path, data_dict)
+    result = _synology_call(syno_api_path, data_dict)
 
     if result['success']:
         print('all ok, diskstation online')
