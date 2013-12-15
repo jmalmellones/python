17875b7a93d2aa90638eb0bacb32e914e437e25e
jmalmellones <jmalmellones@gmail.com>
2013-12-15 21:20:06 +0100

    new setup.py's
    Learning how to do it

---
+++ rss_downloader/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rss_downloader',
+      version='0.1',
+      description='Downloads stuff from elitetorrent.net',
+      url='https://github.com/jmalmellones/python/tree/master/rss_downloader',
+      author='Jose Miguel Almellones',
+      author_email='jmalmellones@gmail.com',
+      license='GPLv2',
+      packages=['rss_downloader'],
+      zip_safe=False)
--- say/setup.py
+++ say/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='say',
       version='0.1',
       description='Says things using Mac OS X say command',
-      url='http://github.com',
+      url='https://github.com/jmalmellones/python/tree/master/say',
       author='Jose Miguel Almellones',
       author_email='jmalmellones@gmail.com',
       license='GPLv2',
-      packages=[],
+      packages=['say'],
       zip_safe=False)
