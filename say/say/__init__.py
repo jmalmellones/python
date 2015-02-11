2898514542f73d93028bb4856a9b112a098a30f1
jmalmellones <jmalmellones@gmail.com>
2015-02-11 20:48:53 +0100

    pequeños retoques
    retocado fichero configuración prowl notifier
    vuelto a poner el say, esta vez usando el altavoz del mac mini

--- say/say/__init__.py
+++ say/say/__init__.py
@@ -1,5 +1,26 @@
 import subprocess
+import datetime
+import bluetooth
+
+week_day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+weekend = ['Saturday', 'Sunday']
+
+
+def se_puede_hablar():
+    now = datetime.datetime.now().time()
+    today = datetime.date.today()
+
+    if today.strftime("%A") in week_day and datetime.time(18) < now < datetime.time(23):
+        return True
+    if today.strftime("%A") in weekend and datetime.time(11) < now < datetime.time(23):
+        return True
+    return False
+
 
 
 def say(sentence):
-    subprocess.call(["say", sentence])
+    if se_puede_hablar():
+        subprocess.call(["say", "-a", "38", sentence])
+    else:
+        print "como no se puede hablar el ", datetime.date.today(), " a las ", datetime.datetime.now().time(), "..."
+        print "susurro: ", sentence
